37d48ad7df901f5b6e5919c1e9a972e65b29db71
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-10-17 20:27:40 +0530

    Logging

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-import 'dotenv/config';
-import express from 'express';
+import "dotenv/config";
+import express from "express";
+import morgan from "morgan";
+import logger from "./logger.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
+const morganFormat = ":method :url :status :response-time ms";
+
+// Middleware to log requests
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+// Application code
 let teaData = [];
 let nextId = 1;
 
